2f6b6dad32c688e0db6f25675e5325ba7ecd7048
marcore94 <marco94.re@gmail.com>
2017-05-16 18:19:35 +0200

    add laser sensor

--- plugin/ElevateFork.cc
+++ plugin/ElevateFork.cc
@@ -2,7 +2,9 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
+#include <gazebo/math/gzmath.hh>
 #include <stdio.h>
+
 namespace gazebo {
 	class ElevateFork : public ModelPlugin {
 		public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
@@ -15,6 +17,12 @@ namespace gazebo {
 
 			//comportamento decente
 			this->fork->SetForce(math::Vector3(0, 0, 13));
+			if(a<500){
+				printf("X = %f\n", this->fork->GetRelativePose().pos.x);
+				printf("Y = %f\n", this->fork->GetRelativePose().pos.y);
+				printf("Z = %f\n", this->fork->GetRelativePose().pos.z);
+				a++;
+			}
 
 			//"130 si vola"
 			//this->fork->SetForce(math::Vector3(0, 0, 130));
@@ -23,6 +31,7 @@ namespace gazebo {
 		private: physics::ModelPtr model;
 		private: event::ConnectionPtr updateConnection;
 		private: physics::LinkPtr fork;
+		private: int a = 0;
 
 	};
 
