d1c75769bb039b08f31196a253f1944b4b97cdf8
Mralx <lonely94@live.it>
2017-07-11 00:29:53 +0200

    Aggiunto messaggio al return se colpisce il muro. Pare funzionare

--- polygons/src/draw_server.cpp
+++ polygons/src/draw_server.cpp
@@ -14,6 +14,7 @@ private:
 	actionlib::SimpleActionServer<polygons::DrawAction> server;
 	ros::Publisher movePub;
 	ros::Subscriber poseSub;
+	turtlesim::Pose previousPose;
 	turtlesim::Pose actualPose;
 	polygons::DrawFeedback feedBack;
 	polygons::DrawResult result;
@@ -49,12 +50,20 @@ void ROSnode::execute(const polygons::DrawGoalConstPtr& goal)
 {
 	ROS_INFO("Goal received, started execution");
 	geometry_msgs::Twist out;
+	bool hit_the_wall = false;
 	float alpha = 2 * boost::math::constants::pi<float>()/goal->sides; //external angle 2pi/n
 	float angularVel = 0.25;
 	float linearVel = 0.25;
 	ros::Time t;
-	for(int i = 0; i < goal->sides; i++)
+
+	for(int i = 0; i < goal->sides && !hit_the_wall; i++) //in questo modo si stoppa dopo aver toccato il muro
 	{
+		//ROS_INFO("LA POSA ATTUALE È x: %f, y: %f, theta: %f", actualPose.x, actualPose.y, actualPose.theta); //riga aggiunta test
+
+		previousPose.x = actualPose.x;
+		previousPose.y = actualPose.y;
+		previousPose.theta = actualPose.theta; //inutile
+
 		t = ros::Time::now();
 		while((ros::Time::now().toSec() - t.toSec()) < alpha/angularVel)
 		{
@@ -71,13 +80,29 @@ void ROSnode::execute(const polygons::DrawGoalConstPtr& goal)
 		}
 		out.linear.x = 0;
 		movePub.publish(out);
-		
+
+		//ROS_INFO("LA POSA ATTUALE È x: %f, y: %f, theta: %f", actualPose.x, actualPose.y, actualPose.theta); //riga aggiunta test
+
 		feedBack.drawn_sides = i + 1;
 		server.publishFeedback(feedBack);
+
+		/** pare funzionare, lo 0.05 è solo per stare sicuri nel caso in cui il turtlesim non giri perfettamente o non vada proprio
+		dritto, anche se non so se sia o meno un caso possibile
+		*/
+		if(!(((actualPose.x - previousPose.x)-(goal->length)*cos(actualPose.theta)) < 0.05 
+					&& ((actualPose.y - previousPose.y) - (goal->length)*sin(actualPose.theta)) < 0.05)){ 
+				//ROS_INFO("DELTA X - R*COS(THETA): %f", (actualPose.x - previousPose.x)-(goal->length)*cos(actualPose.theta));
+				//ROS_INFO("DELTA Y - R*SIN(THETA): %f", (actualPose.y - previousPose.y)-(goal->length)*sin(actualPose.theta));
+				hit_the_wall = true;
+		}
 	}
-	result.success = true;
+	result.success = !hit_the_wall;
 	server.setSucceeded(result);
-	ROS_INFO("Goal achieved");
+	if(!hit_the_wall){
+		ROS_INFO("Goal achieved");
+	}
+	else
+		ROS_INFO("Goal not achieved, wall hit");
 }
 
 void ROSnode::runContinuously() {
